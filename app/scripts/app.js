ed2c227aeab8513e97476b354508ead24459ab9f
Alexander Chernyh <55162957+alexchernyh29@users.noreply.github.com>
2023-02-09 16:27:16 +0300

    Add files via upload

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,20 +2,47 @@ import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
 
 $(() => {
-	svg4everybody();
-
-	$('.group__header').click(function(){
-        $('.list__options').slideToggle(300)
-        $('.group__arrow').toggleClass( "active" );
-    });
-
-    $('.group__header-plan').click(function(){
-        $('.list__options-plan').slideToggle(300);
-        $('.group__arrow-2').toggleClass( "active" );
-    });
-    
-    $('.group__header-floor').click(function(){
-        $('.list__options-floor').slideToggle(300);
-        $('.group__arrow-3').toggleClass( "active" ); 
+    const swiper = new Swiper(".swiper", {
+        slidesPerView: 3,
+        spaceBetween: 1,
+        navigation: {
+            nextEl: ".slider__swiper-button-next-1",
+            prevEl: ".slider__swiper-button-prev-1",
+        },
     });
 });
+
+$('#list-1').click(function(){
+    $('#item-1').slideToggle(300);
+});
+
+$('#list-2').click(function(){
+    $('#item-2').slideToggle(300);
+});
+
+$('#list-3').click(function(){
+    $('#item-3').slideToggle(300);
+});
+
+$('#list-4').click(function(){
+    $('#item-4').slideToggle(300);
+});
+
+$('#list-5').click(function(){
+    $('#item-5').slideToggle(300);
+});
+
+$('#list-6').click(function(){
+    $('#item-6').slideToggle(300);
+});
+
+$('#list-7').click(function(){
+    $('#item-7').slideToggle(300);
+});
+
+$('ul.tabs-case__tab-caption').on('click', 'li:not(.active)', function() {
+    $(this)
+    .addClass('active').siblings().removeClass('active')
+    .closest('div.tabs-case__tab').find('div.tabs-case__tab-content').removeClass('active').eq($(this).index()).addClass('active');
+});
+
